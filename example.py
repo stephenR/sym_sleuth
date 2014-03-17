64e8dfd22bb1c40af476903bcb560e070941eda1
Stephen Röttger <stephen.roettger@gmail.com>
2014-03-17 21:47:44 +0100

    Modified example to include all libc load maps

--- example.py
+++ example.py
@@ -3,30 +3,36 @@
 from sym_sleuth import MemoryELF, ReadException
 
 if __name__ == "__main__":
-  mem_dump = open("ls.libc.bin", "r").read()
-
-  FAIL_MIN = 1794047
-  FAIL_MAX = FAIL_MIN + 2097151 - 1
+  mem_areas = [open("libc.mem{}".format(i), "r").read() for i in range(1,4)]
 
   total_read = 0
 
   def read_cb(addr, sz):
-    if FAIL_MIN <= addr <= FAIL_MAX or addr >= len(mem_dump):
+    if 0x38dbc00000 <= addr < 0x38dbdb6000:
+      mem = mem_areas[0]
+      addr -= 0x38dbc00000
+    elif 0x38dbfb6000 <= addr < 0x38dbfba000:
+      mem = mem_areas[1]
+      addr -= 0x38dbfb6000
+    elif 0x38dbfba000 <= addr < 0x38dbfbc000:
+      mem = mem_areas[2]
+      addr -= 0x38dbfba000
+    else:
       raise ReadException("fail")
 
     global total_read
 
     ret = ""
     while sz > 0:
-      if FAIL_MIN <= addr <= FAIL_MAX or addr >= len(mem_dump):
+      if addr >= len(mem):
         break
-      ret += mem_dump[addr]
+      ret += mem[addr]
       addr += 1
       sz -= 1
     total_read += len(ret)
     return ret
 
-  elf = MemoryELF(read_cb, FAIL_MAX + 20)
+  elf = MemoryELF(read_cb, 0x38dbfba000 + 20)
   print "base: 0x{:x}".format(elf.base)
   print "dynstr: 0x{:x}".format(elf.dynstr_addr)
   print "dynsym: 0x{:x}".format(elf.dynsym_addr)
@@ -36,11 +42,11 @@ if __name__ == "__main__":
   system_addr, _ =  elf.find_symbol("system")
   print "system: 0x{:x}".format(system_addr)
 
-  print "total_read", total_read
+  #print "total_read", total_read
 
-  print "symbols:"
-  for addr, name in elf.iterate_symbols():
-    print "{}: 0x{:x}".format(name, addr)
+  #print "symbols:"
+  #for addr, name in elf.iterate_symbols():
+  #  print "{}: 0x{:x}".format(name, addr)
 
   print "total_read", total_read
 
